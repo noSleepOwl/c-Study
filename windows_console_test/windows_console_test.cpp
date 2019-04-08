3745edfe118cdf837c696a0ac8839bad36e318f6
shigw <844910815@qq.com>
2019-04-08 18:10:33 +0800

    调用控制台命令测试

--- windows_console_test/windows_console_test.cpp
+++ windows_console_test/windows_console_test.cpp
@@ -1,11 +1,17 @@
 ﻿
 #include "pch.h"
 #include<iostream>
+#include<Windows.h>
 using std::cout;
 using std::endl;
 
 void main()
 {
-	cout << "hello world" << endl;
+	for (int i = 0; i < 100; i++) {
+		Sleep(100);
+		system("cls");
+		cout << i << endl;
+	}
+
 }
 
